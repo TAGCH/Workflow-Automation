ca1ea03952ea4cb9be46b711bcee57398cb36439
chamarak <chamarak.g@ku.th>
2024-10-10 16:14:43 +0700

    added workflow page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import Workflow from "./pages/Workflow";
+
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/home" element={<HomePage />} />
+          <Route path="/workflow" element={<Workflow />} />
+          
         </Routes>
       </Router>
   );
