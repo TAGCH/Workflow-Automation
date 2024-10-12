b5c7fa3fbf75312364e7a8ead323f81e392790d9
Canele <winwin6d@hotmail.com>
2024-10-12 21:45:34 +0700

    save change for update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {useDropzone} from 'react-dropzone'
 import HomePage from "./pages/HomePage";
 import Workflow from "./pages/Workflow";
 
