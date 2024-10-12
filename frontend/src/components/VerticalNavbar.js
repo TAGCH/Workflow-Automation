81009782ec9eb4ad2fee39c7f987b6f17eeb97dd
oaoak <khongnat.b@ku.th>
2024-10-12 15:11:58 +0700

    add color for mouse hover

--- frontend/src/components/VerticalNavbar.js
+++ frontend/src/components/VerticalNavbar.js
@@ -24,15 +24,27 @@ const VerticalNavbar = () => {
         >
             <i className={isHovered ? 'bi bi-caret-up-square' : 'bi bi-caret-down-square'} style={{fontSize: '2rem', margin: '10px 0'}}></i>
             <Link to={"/"} style={{ textDecoration: 'none', color: 'inherit', margin: '10px 0'}}>
-                <i className="bi bi-house-door-fill" style={{fontSize: '2rem', margin: '10px 0'}}></i>
+                <i className="bi bi-house-door-fill" style={{fontSize: '2rem', margin: '10px 0'}}
+                   onMouseEnter={(e) => (e.currentTarget.style.color = '#20c997')}
+                   onMouseLeave={(e) => (e.currentTarget.style.color = 'inherit')}>
+                </i>
             </Link>
             <Link to={"/createflow"} style={{ textDecoration: 'none', color: 'inherit', margin: '10px 0'}}>
-                <i className="bi bi-file-earmark-plus-fill" style={{fontSize: '2rem', margin: '10px 0'}}></i>
+                <i className="bi bi-file-earmark-plus-fill" style={{fontSize: '2rem', margin: '10px 0'}}
+                   onMouseEnter={(e) => (e.currentTarget.style.color = '#20c997')}
+                   onMouseLeave={(e) => (e.currentTarget.style.color = 'inherit')}>
+                </i>
             </Link>
             <Link to={"/myflow"} style={{ textDecoration: 'none', color: 'inherit', margin: '10px 0'}}>
-                <i className="bi bi-collection-fill" style={{fontSize: '2rem', margin: '10px 0'}}></i>
+                <i className="bi bi-collection-fill" style={{fontSize: '2rem', margin: '10px 0'}}
+                   onMouseEnter={(e) => (e.currentTarget.style.color = '#20c997')}
+                   onMouseLeave={(e) => (e.currentTarget.style.color = 'inherit')}>
+                </i>
             </Link>
-            <i className="bi bi-chevron-double-left" style={{fontSize: '2rem', margin: '10px 0', marginTop: 'auto'}} onClick={() => previousPage(-1)}></i>
+            <i className="bi bi-chevron-double-left" style={{fontSize: '2rem', margin: '10px 0', marginTop: 'auto'}} onClick={() => previousPage(-1)}
+               onMouseEnter={(e) => (e.currentTarget.style.color = '#20c997')}
+               onMouseLeave={(e) => (e.currentTarget.style.color = 'inherit')}>
+            </i>
         </div>
     );
 };
