3dc365e6ffcd897f55f6d7ed80804b94a09688a4
oaoak <khongnat.b@ku.th>
2024-10-13 13:06:42 +0700

    add the add-contents icon at the middle of the workflow page.

--- frontend/src/pages/WorkflowPage.js
+++ frontend/src/pages/WorkflowPage.js
@@ -7,9 +7,12 @@ import VerticalNavbar from '../components/VerticalNavbar';
 const WorkflowPage = () => {
   return (
       <div>
-        <Navbar />
-        <VerticalNavbar />
-        <Footer />
+          <Navbar/>
+          <VerticalNavbar/>
+          <div className="add-contents">
+              <i className="bi bi-plus-circle-fill add-contents-clickable"></i>
+          </div>
+          <Footer/>
       </div>
   );
 };
