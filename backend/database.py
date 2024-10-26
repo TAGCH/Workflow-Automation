4dcb0e27dcdd350d137ccc7be132f5aa11fbba0b
TAGCH <pisit.au@ku.th>
2024-10-26 23:17:14 +0700

    Add test for services.py

--- backend/database.py
+++ backend/database.py
@@ -9,3 +9,7 @@ engine = create_engine(URL_DATABASE, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def create_database():
+    # This function creates all the tables in the database
+    Base.metadata.create_all(bind=engine)
