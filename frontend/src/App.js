35daa2b041b097efbebdb547bfe8a8513a6ae814
oaoak <khongnat.b@ku.th>
2024-11-03 04:20:52 +0700

    prevent "/" when logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, {useContext} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PrehomePage from "./pages/PrehomePage";
 import HomePage from "./pages/HomePage";
 import GmailWorkflowPage from "./pages/GmailWorkflowPage";
@@ -8,13 +8,18 @@ import MyflowPage from "./pages/MyflowPage";
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import {UserContext} from "./context/UserContext";
 
 
 function App() {
+    const {user} = useContext(UserContext);
   return (
       <Router>
         <Routes>
-            <Route path="/" element={<PrehomePage />} />
+            <Route
+                path="/"
+                element={user ? <Navigate to={`/home/${user.id}`} replace /> : <PrehomePage />}
+            />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route
